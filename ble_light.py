3be746f7cba8e6d2ddf23ba5c55d6dfe26ccb272
Inderpreet Singh <er.inderpreet@gmail.com>
2020-04-16 17:37:31 -0400

    Added RGBW to MQTT

--- ble_light.py
+++ ble_light.py
@@ -5,7 +5,7 @@ import paho.mqtt.client as mqtt
 
 LIGHT01 = "20:C3:8F:8D:8C:9E"
 VALUE = ["00000000", "FF000000", "00FF0000", "0000FF00", "000000FF", "00000000"];
-i=1
+RGBW = "00000000"
 
 child = pexpect.spawn("gatttool -I")
 
@@ -23,13 +23,11 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    global i
+    global RGBW
     print(msg.topic+" "+str(msg.payload))
-    if str(msg.payload).find('ON') >= 0:
-        i=1
-    else:
-        i=0 
-
+    l = len((str(msg.payload)))
+    if l == 8:
+        RGBW = str(msg.payload)
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -38,7 +36,7 @@ client.connect_async("mqtt.eclipse.org", 1883, 60)
 client.loop_start()
 while True:
 	# for i in range(6):
-	child.sendline("char-write-req 0x0031 {0}".format(VALUE[i]))
+	child.sendline("char-write-req 0x0031 {0}".format(RGBW)) #VALUE[i]))
 	child.expect("Characteristic value was written successfully", timeout=5)
 	time.sleep(1);
 
