5a5d924dcc6cd80b214c6ea96d5c1489adfe137e
Inderpreet Singh <er.inderpreet@gmail.com>
2020-04-17 22:58:59 -0400

    Works with BLE Light and MQTT

--- ble_light.py
+++ ble_light.py
@@ -19,7 +19,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("ip_v1/ETEC224/ble_light_red")
+    client.subscribe("ip_v1/BLE_light_all/RGBW")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
